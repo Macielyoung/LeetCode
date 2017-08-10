8ac41da10b532762d01a84685cb48049895356df
Macielyoung <291938109@qq.com>
2017-08-10 21:41:49 +0800

    Add files via upload

---
+++ 23. Merge k Sorted Lists/1.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int sz = lists.size();
+        if(sz == 0) return NULL;
+        
+        //二分合并
+        while(sz>1)
+        {
+            int k = (sz+1)/2;
+            for(int i=0;i<sz/2;i++)
+            {
+                lists[i] = mergeTwoLists(lists[i],lists[i+k]);
+            }
+            sz = k;
+        }
+        return lists[0];
+    }
+    
+    //合并两个排序的链表
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2)
+    {
+        if(!l1) return l2;
+        if(!l2) return l1;
+        
+        ListNode* start;
+        ListNode* cur;
+        if(l1->val < l2->val)
+        {
+            start = cur = l1;
+            l1 = l1->next;
+        }
+        else{
+            start = cur = l2;
+            l2 = l2->next;
+        }
+        while(l1 != NULL && l2 != NULL)
+        {
+            if(l1->val < l2->val)
+            {
+                cur->next = l1;
+                cur = l1;
+                l1 = l1->next;
+            }
+            else{
+                cur->next = l2;
+                cur = l2;
+                l2 = l2->next;
+            }
+        }
+        if(l1 != NULL)
+        {
+            cur->next = l1;
+        }
+        else{
+            cur->next = l2;
+        }
+        return start;
+    }
+};
