23c8ee961f1f9955f085f36803e1a4ff5dec79cc
Maciel <maciel@promote.cache-dns.local>
2018-07-23 00:31:43 +0800

    add non-recursive way(BFS)

--- 112. Path Sum/pathSum.py
+++ 112. Path Sum/pathSum.py
@@ -7,6 +7,7 @@ class TreeNode:
         self.right = None
 
 class Solution:
+    # recursive
     def hasPathSum(self, root, sum):
         """
         :type root: TreeNode
@@ -23,6 +24,46 @@ class Solution:
         left = self.hasPathSum(root.left, sum - root.val)
         right = self.hasPathSum(root.right, sum - root.val)
         return (left or right)
+
+    # non-recursive
+    def hasPathSum2(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if root == None:
+            return False
+
+        # One queue to maintain Nodes
+        # Second to maintain sum upto that node
+        q1 = []
+        q2 = []
+
+        q1.append(root)
+        q2.append(root.val)
+
+        while (q1):
+
+            ql = len(q1)
+            for i in range(0, ql):
+
+                a = q1.pop(0)
+                b = q2.pop(0)
+
+                # If node sum is equal to sum and is a leaf node then return True
+                if (b == sum and a.left == None and a.right == None):
+                    return True
+
+                if (a.left != None):
+                    q1.append(a.left)
+                    q2.append(b + a.left.val)
+
+                if (a.right != None):
+                    q1.append(a.right)
+                    q2.append(b + a.right.val)
+
+        return False
 
 if __name__ == '__main__':
     solu = Solution()
@@ -46,5 +87,5 @@ if __name__ == '__main__':
     X = TreeNode(1)
     Y = TreeNode(2)
     X.left = Y
-    existence = solu.hasPathSum(X, 1)
+    existence = solu.hasPathSum2(A, 22)
     print existence
--- 112. Path Sum/problem.md
+++ 112. Path Sum/problem.md
@@ -25,3 +25,4 @@ return true, as there exist a root-to-leaf path `5->4->11->2` which sum is 22.
 ## Solution
 
 * 递归法。当该节点为空时返回False。当该节点为叶节点，同时值等于sum则返回True，不等则返回False。
+* 使用堆栈，宽度优先遍历，每层中的到叶节点的和如果为sum，则返回True。
