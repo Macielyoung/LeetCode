12605096ec5d549962a1e09d20175c277463bd0e
Macielyoung <291938109@qq.com>
2018-08-14 12:42:44 +0800

    count 5

---
+++ 172. Factorial Trailing Zeroes/problem.md
@@ -0,0 +1,25 @@
+## Problem
+
+Given an integer *n*, return the number of trailing zeroes in *n*!.
+
+**Example 1:**
+
+```
+Input: 3
+Output: 0
+Explanation: 3! = 6, no trailing zero.
+```
+
+**Example 2:**
+
+```
+Input: 5
+Output: 1
+Explanation: 5! = 120, one trailing zero.
+```
+
+
+
+## Solution
+
+* 统计5的数量。
---
+++ 172. Factorial Trailing Zeroes/zero.py
@@ -0,0 +1,20 @@
+#-*- coding: UTF-8 -*-
+import math
+
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        while(n > 0):
+            n //= 5
+            count += n
+        return count
+
+if __name__ == '__main__':
+    solu = Solution()
+    n = 25
+    res = solu.trailingZeroes(n)
+    print(res)
