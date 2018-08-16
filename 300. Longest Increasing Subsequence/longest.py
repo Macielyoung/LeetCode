0785511468a22d8dbf884e7f33f05dd3b17330bf
Macielyoung <291938109@qq.com>
2018-08-16 15:29:23 +0800

    dp

---
+++ 300. Longest Increasing Subsequence/longest.py
@@ -0,0 +1,23 @@
+#-*- coding: UTF-8 -*-
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        dp = [1 for i in range(len(nums))]
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if(nums[i] > nums[j]):
+                    if dp[j]+1 > dp[i]:
+                        dp[i] = dp[j]+1
+        return max(dp)
+
+if __name__ == '__main__':
+    solu = Solution()
+    nums = [2]
+    res = solu.lengthOfLIS(nums)
+    print(res)
