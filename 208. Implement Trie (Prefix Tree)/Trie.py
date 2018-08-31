073ca141609be8e72bca69713956b1c137a0bfdd
Macielyoung <291938109@qq.com>
2018-08-31 14:36:29 +0800

    use a treenode dict

---
+++ 208. Implement Trie (Prefix Tree)/Trie.py
@@ -0,0 +1,71 @@
+#-*- coding: UTF-8 -*-
+from collections import defaultdict
+
+class TrieNode:
+    # Initialize your data structure here.
+    def __init__(self):
+        self.children = defaultdict(TrieNode)
+        self.is_word = False
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: void
+        """
+        current = self.root
+        for letter in word:
+            current = current.children[letter]
+        current.is_word = True
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        current = self.root
+        for letter in word:
+            current = current.children.get(letter)
+            if current is None:
+                return False
+        return current.is_word
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        current = self.root
+        for letter in prefix:
+            current = current.children.get(letter)
+            if current is None:
+                return False
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+
+if __name__ == '__main__':
+    trie = Trie()
+
+    trie.insert("apple")
+    res1 = trie.search("apple")
+    res2 = trie.search("app")
+    res3 = trie.startsWith("app")
+    trie.insert("app")
+    res4 = trie.search("app")
+    print(res1, res2, res3, res4)
