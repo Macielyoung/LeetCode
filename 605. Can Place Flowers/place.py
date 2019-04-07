ac5d1297b3465c3c7f3d0fc5a873235db69a3a7e
Macielyoung <291938109@qq.com>
2019-04-07 22:06:08 +0800

    traverse with limited condition

--- 605. Can Place Flowers/place.py
+++ 605. Can Place Flowers/place.py
@@ -3,27 +3,31 @@ import math
 
 class Solution:
     def canPlaceFlowers(self, flowerbed, n):
-        if not flowerbed and n > 0:
-            return False
-        empty = []
-        count = 0
-        for index, value in enumerate(flowerbed):
-            if count > 0:
-                if value == 1 or value == 0 and index == len(flowerbed)-1:
-                    empty.append(count)
-            if value == 1:
-                count = 0
-            else:
+        pos, count = 0, 0
+        length = len(flowerbed)
+        while pos < length:
+            if (flowerbed[pos]==0)and(pos==0 or flowerbed[pos-1]==0)and(pos==length-1 or flowerbed[pos+1]==0):
+                flowerbed[pos] = 1
                 count += 1
-        all_count = sum([math.ceil(i/2)-1 for i in empty])
-        if all_count >= n:
-            return True
-        else:
-            return False
+            pos += 1
+        return count >= n
+    
+    def canPlaceFlowers2(self, flowerbed, n):
+        pos, count = 0, 0
+        length = len(flowerbed)
+        while pos < length:
+            if (flowerbed[pos]==0)and(pos==0 or flowerbed[pos-1]==0)and(pos==length-1 or flowerbed[pos+1]==0):
+                flowerbed[pos] = 1
+                pos += 1
+                count += 1
+            if count >= n:
+                return True
+            pos += 1
+        return False
 
 if __name__ == '__main__':
     solu = Solution()
-    flowerbed = [1,0,1,0,1]
+    flowerbed = [0,0,1,0,1]
     n = 1
-    res = solu.canPlaceFlowers(flowerbed, n)
+    res = solu.canPlaceFlowers2(flowerbed, n)
     print(res)
